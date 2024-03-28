55347a1d2ef6407cc73f5f3f1e84629bef123f9c
Igor moreira <94862012+igormorantos@users.noreply.github.com>
2024-03-28 14:15:44 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -12,7 +12,7 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 # Dar permissões necessárias
 sudo chmod +x /usr/local/bin/docker-compose
 # Dar um curl no arquivo .yaml do meu git-hub e criar um arquivo de mesmo nome contendo seu conteúdo
-sudo curl -o dockerCompose.yaml "https://raw.githubusercontent.com/igormorantos/Aws-Docker/main/dockerCompose.yaml"
+sudo curl -o dockerCompose.yaml "https://raw.githubusercontent.com/igormorantos/Aws-Docker/blob/main/docker-compose.yaml"
 # Instalar cliente nfs 
 sudo sudo yum install -y amazon-efs-utils
 # Criar diretório para montagem
@@ -20,9 +20,9 @@ sudo mkdir /efs
 # Dar permissões necessárias ao diretório ( leitura, escrita e execução ) 
 sudo chmod +rwx /efs
 # Montar o sistema de arquivos com o EFS
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0647fdec6707f2113.efs.us-east-1.amazonaws.com:/ /mnt/efs/
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0c2450fab7143e6e8.efs.us-east-1.amazonaws.com:/ /efs
 # Habilitar montagem automatica quando a máquina inicializar
-echo "fs-07c68e847f4ea9744.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
+echo "fs-0c2450fab7143e6e8.efs.us-east-1.amazonaws.com:/ /efs nfs defaults 0 0" >> /etc/fstab
 # Adicionar o usuário atual no grupo do docker
 usermod -aG docker ${USER}
 # Dar permissão de leitura e escrita no docker.sock
